25e8b2e8be3aa44f119c9819ca3afe3f17809e05
sawoffer <sawoffer@gmail.com>
2016-04-03 09:05:35 +0300

    Added more comments to ubc.sh

--- ubc.sh
+++ ubc.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-CTID="$1" # Номер вдски, передаваемый заббиксом.
-RES="$2"  # Запрашеваемый ресурс (например numproc)
-TYPE="$3" # Тип ресурса (2 - held, 3 - maxheld, 4 - barrier, 5 - limit, 6 - failcn)
+## This script is for zabbix-agend driven host with OpenVZ containers
+## It gets info about OpenVZ containers by parising /proc/user_beancounters
+## Exec it on host runing OpenVZ, not inside OpenVZ container.
+
+CTID="$1" # OpenVZ container id.
+RES="$2"  # Param to check (e.g. numproc). For complete list see /proc/user_beancounters
+TYPE="$3" # Param type (2 - held, 3 - maxheld, 4 - barrier, 5 - limit, 6 - failcnt)
 
 if [ "$RES" = "kmemsize" ]
         then
