015f8d58685e68212b823bf69cc86a340a839135
sawoffer <sawoffer@gmail.com>
2016-04-20 15:01:14 +0300

    Date-time delimiter changed from "-" to " ".

--- mysql_per_table_dump.sh
+++ mysql_per_table_dump.sh
@@ -9,21 +9,21 @@ mysql=`which mysql`
 mysqldump=`which mysqldump`
 gzip=`which gzip`
 
-$echo "`$date +%F-%H:%M:%S` mysqldump begin" >> $log 2>&1
+$echo "`$date +%F %H:%M:%S` mysqldump begin" >> $log 2>&1
 for db in `$mysql -ABNe "show databases;"`
     do
         dbdir=/adv/backup/mysql-dump/`$date +%F`/$db
-        $echo "`$date +%F-%H:%M:%S` dumping db $db" >> $log 2>&1
-#        $echo "`$date +%F-%H:%M:%S` creating dir $dir" >> $log 2>&1
+        $echo "`$date +%F %H:%M:%S` dumping db $db" >> $log 2>&1
+#        $echo "`$date +%F %H:%M:%S` creating dir $dir" >> $log 2>&1
         $mkdir -p $dir >> $log 2>&1
         for t in `$mysql -D $db -ABNe "show tables;"`
             do
-#                $echo "`$date +%F-%H:%M:%S` creating dir $dbdir" >> $log 2>&1
+#                $echo "`$date +%F %H:%M:%S` creating dir $dbdir" >> $log 2>&1
                 $mkdir -p $dbdir
-                $echo "`$date +%F-%H:%M:%S` dumping table $t" >> $log 2>&1
+                $echo "`$date +%F %H:%M:%S` dumping table $t" >> $log 2>&1
                 $mysqldump $db $t | $gzip > $dbdir/$t.gz
-                $echo "`$date +%F-%H:%M:%S` done $t" >> $log 2>&1
+                $echo "`$date +%F %H:%M:%S` done $t" >> $log 2>&1
         done >> $log 2>&1
-        $echo "`$date +%F-%H:%M:%S` done db $db" >> $log 2>&1
+        $echo "`$date +%F %H:%M:%S` done db $db" >> $log 2>&1
     done
-$echo "`$date +%F-%H:%M:%S` mysqldump complete" >> $log 2>&1
+$echo "`$date +%F %H:%M:%S` mysqldump complete" >> $log 2>&1
