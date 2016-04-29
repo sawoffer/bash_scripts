8bec643d1819957b686e8885f7bf88811f5f6699
root <root@debian-wheezy.vagrantup.com>
2016-04-29 10:57:58 +0000

    switched from  to

--- mongodump.sh
+++ mongodump.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo=`which echo`
-mongodump=`which mongodump`
+echo=$(which echo)
+mongodump=$(which mongodump)
 dir=/adv/backup/mongo
 log=/var/log/mongodump.log
 
-$echo "`date +%F\ %H:%M:%S` mongodump begin" >> $log
-$mongodump -u root -p password --archive=$dir/`date +%F`-mongodump.gz --gzip >> $log 2>&1
-$echo "`date +%F\ %H:%M:%S` mongodump end" >> $log
+$echo "$(date +%F\ %H:%M:%S) mongodump begin" >> $log
+$mongodump -u root -p password --archive=$dir/$(date +%F)-mongodump.gz --gzip >> $log 2>&1
+$echo "$(date +%F\ %H:%M:%S) mongodump end" >> $log
