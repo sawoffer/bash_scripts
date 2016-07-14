b8cdb8f7f8190c01d836aa36dafc6017bf492632
sawoffer <sawoffer@gmail.com>
2016-07-14 13:31:46 +0300

    Update mysql_per_table_dump.sh

--- mysql_per_table_dump.sh
+++ mysql_per_table_dump.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 date=$(which date)
-dir=/adv/backup/mysql-dump/$($date +%F)
+dir=/adv/backup/mysql_dump/$($date +%F)
 log=/var/log/mysqldump.log
 echo=$(which echo)
 mkdir=$(which mkdir)
@@ -12,7 +12,7 @@ gzip=$(which gzip)
 $echo "$($date +%F\ %H:%M:%S) mysqldump begin" >> $log 2>&1
 for db in $($mysql -ABNe "show databases;")
     do
-        dbdir=/adv/backup/mysql-dump/$($date +%F)/$db
+        dbdir=/adv/backup/mysql_dump/$($date +%F)/$db
         $echo "$($date +%F\ %H:%M:%S) dumping db $db" >> $log 2>&1
         $mkdir -p $dir >> $log 2>&1
         for t in $($mysql -D $db -ABNe "show tables;")
@@ -21,7 +21,7 @@ for db in $($mysql -ABNe "show databases;")
                 $echo "$($date +%F\ %H:%M:%S) dumping table $t" >> $log 2>&1
                 $mysqldump $db $t | $gzip > $dbdir/$t.gz
                 $echo "$($date +%F\ %H:%M:%S) done $t" >> $log 2>&1
-        done >> $log 2>&1
+            done >> $log 2>&1
         $echo "$($date +%F\ %H:%M:%S) done db $db" >> $log 2>&1
     done
 $echo "$($date +%F\ %H:%M:%S) mysqldump complete" >> $log 2>&1
