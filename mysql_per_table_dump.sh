502440401360f62cc6cb3d34f811bd68155fd29a
sawoffer <sawoffer@gmail.com>
2017-12-07 20:24:48 +0300

    Update mysql_per_table_dump.sh

--- mysql_per_table_dump.sh
+++ mysql_per_table_dump.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 
 date=$(which date)
-dir=/adv/backup/mysql_dump/$($date +%F)
+cdate=$($date +%F)
+now=$($date +%F\ %H:%M:%S)
+dir=/adv/backup/mysql_dump/
 log=/var/log/mysqldump.log
 echo=$(which echo)
 mkdir=$(which mkdir)
 mysql=$(which mysql)
 mysqldump=$(which mysqldump)
 gzip=$(which gzip)
+rm=$(which rm)
 
-$echo "$($date +%F\ %H:%M:%S) mysqldump begin" >> $log 2>&1
+$echo "$now cleaning directory from previous backup" >> $log 2>&1
+$rm -rf $dir/*
+$echo "$now mysqldump begin" >> $log 2>&1
 for db in $($mysql -ABNe "show databases;")
     do
-        dbdir=/adv/backup/mysql_dump/$($date +%F)/$db
-        $echo "$($date +%F\ %H:%M:%S) dumping db $db" >> $log 2>&1
+        dbdir=$dir/$cdate/$db
+        $echo "$now dumping db $db" >> $log 2>&1
         $mkdir -p $dir >> $log 2>&1
         for t in $($mysql -D $db -ABNe "show tables;")
             do
                 $mkdir -p $dbdir
-                $echo "$($date +%F\ %H:%M:%S) dumping table $t" >> $log 2>&1
-                $mysqldump $db $t | $gzip > $dbdir/$t.gz
-                $echo "$($date +%F\ %H:%M:%S) done $t" >> $log 2>&1
-            done >> $log 2>&1
-        $echo "$($date +%F\ %H:%M:%S) done db $db" >> $log 2>&1
+                $echo "$now dumping table $t" >> $log 2>&1
+                $mysqldump --single-transaction $db $t | $gzip > $dbdir/$t.gz
+                $echo "$now done $t" >> $log 2>&1
+        done >> $log 2>&1
+        $echo "$now done db $db" >> $log 2>&1
     done
-$echo "$($date +%F\ %H:%M:%S) mysqldump complete" >> $log 2>&1
+$echo "$now mysqldump complete" >> $log 2>&1
